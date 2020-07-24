60a45bcd4a35dcd1e871c075c4c8fd9239ddbbbd
Muqker <muqker@muqker.com>
2020-07-24 19:24:28 +0300

    play with part1 hello world

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name} of {props.age} months</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+        <Hello name="Alfie" age={b-a} />
+      </p>
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
