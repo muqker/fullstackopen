ac922b1c4152fff0134ea30fa69a29263a321cb2
Muqker <muqker@muqker.com>
2020-07-28 15:56:35 +0300

    removed unwanted code from phonebook

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  console.log(response.data)
-})
 
 ReactDOM.render(<App />, document.getElementById('root'))
